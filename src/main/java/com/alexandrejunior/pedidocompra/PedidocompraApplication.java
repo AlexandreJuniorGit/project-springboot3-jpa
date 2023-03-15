17ab5370c5f4448c141a9048f8b101c590acc953
Alexandre Aparecido <aadsj.alexandre@gmail.com>
2023-03-15 14:41:48 -0900

    Project created

---
+++ src/main/java/com/alexandrejunior/pedidocompra/PedidocompraApplication.java
@@ -0,0 +1,13 @@
+package com.alexandrejunior.pedidocompra;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PedidocompraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PedidocompraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/alexandrejunior/pedidocompra/PedidocompraApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexandrejunior.pedidocompra;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PedidocompraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
