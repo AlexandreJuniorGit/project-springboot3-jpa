a586062252584085eac3c92f63138a228276f153
Alexandre Aparecido <aadsj.alexandre@gmail.com>
2023-03-16 12:53:27 -0900

    Added Category entity(repository, services, resources and seeding)

---
+++ src/main/java/com/alexandrejunior/pedidocompra/services/CategoryService.java
@@ -0,0 +1,26 @@
+package com.alexandrejunior.pedidocompra.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.alexandrejunior.pedidocompra.entities.Category;
+import com.alexandrejunior.pedidocompra.repositories.CategoryRepository;
+
+@Service
+public class CategoryService {
+	
+	@Autowired
+	private CategoryRepository repository;
+	
+	public List<Category> findAll(){
+		return repository.findAll();
+	}
+	
+	public Category findById(Long id) {
+		Optional<Category> obj = repository.findById(id);
+		return obj.get();
+	}
+}
