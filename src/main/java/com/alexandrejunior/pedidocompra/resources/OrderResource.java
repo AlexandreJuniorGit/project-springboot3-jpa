4f50fa99bf56a1e4b8fc3a8939fb674a22cdf6d9
Alexandre Aparecido <aadsj.alexandre@gmail.com>
2023-03-16 10:43:01 -0900

    Added Order entity(repository, services and seeding), Instant(ISO 8601)

--- src/main/java/com/alexandrejunior/pedidocompra/resources/OrderResource.java
+++ src/main/java/com/alexandrejunior/pedidocompra/resources/OrderResource.java
@@ -9,25 +9,25 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.alexandrejunior.pedidocompra.entities.User;
-import com.alexandrejunior.pedidocompra.services.UserService;
+import com.alexandrejunior.pedidocompra.entities.Order;
+import com.alexandrejunior.pedidocompra.services.OrderService;
 
 @RestController
-@RequestMapping(value = "/users")
-public class UserResource {
-	
+@RequestMapping(value = "/orders")
+public class OrderResource {
+
 	@Autowired
-	private UserService service;
+	private OrderService service;
 	
 	@GetMapping
-	public ResponseEntity<List<User>> finAll(){
-		List<User> list = service.findAll();
+	public ResponseEntity<List<Order>> findAll(){
+		List<Order> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
 	
-	@GetMapping(value="/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id) {
-		User obj = service.findByid(id);
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<Order> findById(@PathVariable Long id){
+		Order obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
 	}
 }
