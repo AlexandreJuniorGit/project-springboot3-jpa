51618e268ffdcbe6e2741b6be48f2a6bc4f2a3f8
Alexandre Aparecido <aadsj.alexandre@gmail.com>
2023-03-17 12:49:40 -0900

    Payment, one-to-one association

--- src/main/java/com/alexandrejunior/pedidocompra/entities/Order.java
+++ src/main/java/com/alexandrejunior/pedidocompra/entities/Order.java
@@ -9,6 +9,7 @@ import java.util.Set;
 import com.alexandrejunior.pedidocompra.entities.enums.OrderStatus;
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,6 +17,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -39,9 +41,12 @@ public class Order implements Serializable{
 	@OneToMany(mappedBy = "id.order")
 	private Set<OrderItem> items = new HashSet<>();
 	
+	@OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+	private Payment payment;
+	
 	public Order() {
 	}
-
+	
 	public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
 		super();
 		this.id = id;
@@ -49,19 +54,19 @@ public class Order implements Serializable{
 		setOrderStatus(orderStatus);
 		this.client = client;
 	}
-
+	
 	public Long getId() {
 		return id;
 	}
-
+	
 	public void setId(Long id) {
 		this.id = id;
 	}
-
+	
 	public Instant getMoment() {
 		return moment;
 	}
-
+	
 	public void setMoment(Instant moment) {
 		this.moment = moment;
 	}
@@ -69,19 +74,27 @@ public class Order implements Serializable{
 	public OrderStatus getOrderStatus() {
 		return OrderStatus.valueOf(orderStatus);
 	}
-
+	
 	public void setOrderStatus(OrderStatus orderStatus) {
 		if(orderStatus != null) {
 			this.orderStatus = orderStatus.getCode();
 		}
 	}
-
+	
 	public User getClient() {
 		return client;
 	}
-
+	
 	public void setClient(User client) {
 		this.client = client;
+	}
+	
+	public Payment getPayment() {
+		return payment;
+	}
+
+	public void setPayment(Payment payment) {
+		this.payment = payment;
 	}
 
 	public Set<OrderItem> getItems(){
@@ -92,7 +105,7 @@ public class Order implements Serializable{
 	public int hashCode() {
 		return Objects.hash(id);
 	}
-
+	
 	@Override
 	public boolean equals(Object obj) {
 		if (this == obj)
