5111c9d67d6d5eff950918f57eb873f15ad865cc
Alexandre Aparecido <aadsj.alexandre@gmail.com>
2023-03-17 14:45:09 -0900

    Exception handling - update

--- src/main/java/com/alexandrejunior/pedidocompra/services/UserService.java
+++ src/main/java/com/alexandrejunior/pedidocompra/services/UserService.java
@@ -13,6 +13,8 @@ import com.alexandrejunior.pedidocompra.repositories.UserRepository;
 import com.alexandrejunior.pedidocompra.services.exceptions.DatabaseException;
 import com.alexandrejunior.pedidocompra.services.exceptions.ResourceNotFoundException;
 
+import jakarta.persistence.EntityNotFoundException;
+
 @Service
 public class UserService {
 
@@ -43,9 +45,13 @@ public class UserService {
 	}
 	
 	public User update(Long id, User obj) {
-		User entity = repository.getReferenceById(id);
-		updateData(entity, obj);
-		return repository.save(entity);
+		try {
+			User entity = repository.getReferenceById(id);
+			updateData(entity, obj);
+			return repository.save(entity);
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException(id);
+		}
 	}
 
 	private void updateData(User entity, User obj) {
